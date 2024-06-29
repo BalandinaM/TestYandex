f295494e01cf76ffe47fa992f29a8b0919966bb7
BalandinaM <marina.balandina.89@mail.ru>
2024-06-29 13:59:38 +0500

    бегущая строка

--- runLine.js
+++ runLine.js
@@ -1,18 +1,49 @@
-const wrapRunLine = document.querySelectorAll('.wrap-runLine');
+const runLine = document.querySelectorAll('.runLine');
 
-for (let elem of wrapRunLine) {
-  const sloganFirst = document.createElement("p");
-  sloganFirst.className = "runLine__text";
-  sloganFirst.innerHTML =
-    "Шахматы двигают вперёд не только культуру, но и экономику!";
-  const sloganSecond = document.createElement("p");
-  sloganSecond.className = "runLine__text";
-  sloganSecond.innerHTML = "Лёд тронулся, господа присяжные заседатели!";
-  const span = document.createElement("span");
-  span.className = "runLine__span";
+// for (let elem of wrapRunLine) {
+//   const sloganFirst = document.createElement("p");
+//   sloganFirst.className = "runLine__text";
+//   sloganFirst.innerHTML =
+//     "Шахматы двигают вперёд не только культуру, но и экономику!<span></span>";
+//   const sloganSecond = document.createElement("p");
+//   sloganSecond.className = "runLine__text";
+//   sloganSecond.innerHTML = "Лёд тронулся, господа присяжные заседатели!<span></span>";
+//   const sloganThird = document.createElement("p");
+//   sloganThird.className = "runLine__text";
+//   sloganThird.innerHTML = "Спасение утопающих, дело рук самих утопающих!<span></span>";
 
-  elem.append(sloganFirst);
-  elem.append(span);
-  elem.append(sloganSecond);
+//   elem.append(sloganFirst);
+//   elem.append(sloganSecond);
+//   elem.append(sloganThird);
+// }
+
+///////////////////////////////////////////////////////////////////
+
+//Работает, но дергается, и спан игнорируется
+
+for (let elem of runLine) {
+  let text = document.createElement("div");
+  text.className = "runLine__text";
+  text.innerHTML = "&nbsp;Шахматы двигают вперёд не только культуру, но и экономику! <span></span> Лёд тронулся, господа присяжные заседатели! <span></span> Спасение утопающих, дело рук самих утопающих!<span></span>";
+  elem.append(text);
 }
+
+//const runLine = document.querySelectorAll('.runLine__text');
+
+function playh1(element){
+  let str = element.textContent;
+  let a = str.slice(1);
+  let b = str.slice(0, 1);
+  let newstr = a + b;
+  element.textContent = newstr;
+
+  return;
+};
+
+// for (let elem of runLine) {
+//   let si = setInterval(playh1, 50, elem);
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 
